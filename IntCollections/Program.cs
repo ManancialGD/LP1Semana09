59d65eec4102a3e1d4c2027a6bb49306a3a2c368
“[Breno <manancialgd@gmail.com>
2024-05-10 17:28:47 +0100

    Add the number collections to the collections

--- IntCollections/Program.cs
+++ IntCollections/Program.cs
@@ -5,13 +5,25 @@ namespace IntCollections
 {
     class Program
     {
-        static private List<int> intList;
+        static private List<int> intList = new List<int>{1, 10, -30, 10, -5};
         static private Stack<int> intStack;
         static private Queue<int> intQueue;
-        static private HashSet<int> intHashSet;
+        static private HashSet<int> intHashSet = new HashSet<int>{1, 10, -30, 10, -5};
         static void Main(string[] args)
         {
-            
+            // Add items to the Stack
+            intStack.Push(1);
+            intStack.Push(10);
+            intStack.Push(-30);
+            intStack.Push(10);
+            intStack.Push(-5);
+
+            // Add items to the Queue
+            intQueue.Enqueue(1);
+            intQueue.Enqueue(10);
+            intQueue.Enqueue(-30);
+            intQueue.Enqueue(10);
+            intQueue.Enqueue(-5);
         }
     }
 }
