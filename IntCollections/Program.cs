2b0e66d93e3335caaed3128440f8b4194e8aa835
“[Breno <manancialgd@gmail.com>
2024-05-10 17:21:21 +0100

    Created a List, Stack, Queue, HashSet

--- IntCollections/Program.cs
+++ IntCollections/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace IntCollections
 {
     class Program
     {
+        static private List<int> intList;
+        static private Stack<int> intStack;
+        static private Queue<int> intQueue;
+        static private HashSet<int> intHashSet;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
